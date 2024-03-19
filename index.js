8ecf2140ed438b75aa935fe75120015499a55013
julianatonu <julianatonu@gmail.com>
2024-03-19 15:45:09 +0600

    get all cadidates data

--- index.js
+++ index.js
@@ -25,12 +25,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const  candidatesCollection =client.db('candidatesDB').collection('candidates');
+
+app.get('/candidates', async(req,res)=>{
+    const cursor = candidatesCollection.find();
+const result = await  cursor.toArray();
+res.send(result);
+})
+
+// app.get('/menu',async(req,res)=>{
+//   const result =await menuCollection.find().toArray();
+//   res.send(result);
+// })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
